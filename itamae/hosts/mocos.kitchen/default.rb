187ff2ecbacdaac6ac9892ebbe12f1ae35368d47
tyage <namatyage@gmail.com>
2017-04-17 23:03:45 +0900

    fixed parse error

--- itamae/hosts/mocos.kitchen/default.rb
+++ itamae/hosts/mocos.kitchen/default.rb
@@ -5,9 +5,9 @@ node.reverse_merge!(
   letsencrypt: {
     certbot_auto_path: '/usr/bin/certbot-auto',
     email: 'namatyage@gmail.com',
-    cron_user: root
+    cron_user: 'root',
     cron_file_path: '/etc/cron.d/itamae-letsencrypt',
-    cron_configuration: true
+    cron_configuration: true,
     challenge_type: 'http-01',
     domains: ['mocos.kitchen'],
     authenticator: 'webroot',
@@ -18,7 +18,7 @@ node.reverse_merge!(
 
 include_cookbook 'basic'
 
-include_cookbook 'docker'
-include_cookbook 'l2tp_ipsec_vpn_server'
+#include_cookbook 'docker'
+#include_cookbook 'l2tp_ipsec_vpn_server'
 
 include_recipe 'letsencrypt::get'

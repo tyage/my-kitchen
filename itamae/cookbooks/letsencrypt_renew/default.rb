3f5600ad89ba9823c0935afd69282fbf0cb3593f
tyage <namatyage@gmail.com>
2017-04-18 16:29:14 +0900

    renewal with webroot (not standalone)

---
+++ itamae/cookbooks/letsencrypt_renew/default.rb
@@ -0,0 +1,30 @@
+options = [
+  "-a #{node[:letsencrypt_renew][:authenticator]}"
+]
+options << "-w #{node[:letsencrypt_renew][:webroot_path]}" if node[:letsencrypt_renew][:webroot_path]
+options << "--post-hook \"#{node[:letsencrypt_renew][:post_hook]}\"" if node[:letsencrypt_renew][:post_hook]
+
+template '/etc/systemd/system/certbot.service' do
+  action :create
+  mode '0644'
+  owner 'root'
+  group 'root'
+  variables(options: options.join(' '))
+  notifies :run, 'execute[systemctl daemon-reload]'
+end
+
+execute 'systemctl daemon-reload' do
+  action :nothing
+end
+
+remote_file '/etc/systemd/system/certbot.timer' do
+  action :create
+  mode '0644'
+  owner 'root'
+  group 'root'
+  notifies :run, 'execute[systemctl enable certbot.timer]'
+end
+
+execute 'systemctl enable certbot.timer' do
+  action :nothing
+end
--- itamae/hosts/mocos.kitchen/default.rb
+++ itamae/hosts/mocos.kitchen/default.rb
@@ -1,9 +1,11 @@
+certbot_auto_path = '/usr/bin/certbot-auto'
+
 node.reverse_merge!(
   docker: {
     users: ['tyage']
   },
   letsencrypt: {
-    certbot_auto_path: '/usr/bin/certbot-auto',
+    certbot_auto_path: certbot_auto_path,
     email: 'namatyage@gmail.com',
     cron_configuration: false,
     challenge_type: 'http-01',
@@ -12,14 +14,16 @@ node.reverse_merge!(
     debug_mode: false
   },
   letsencrypt_renew: {
-    certbot_auto_path: '/usr/bin/certbot-auto',
+    certbot_auto_path: certbot_auto_path,
+    authenticator: 'webroot',
+    webroot_path: '/tmp/letsencrypt-auto',
     post_hook: '/usr/sbin/service nginx reload'
   }
 )
 
 include_cookbook 'basic'
 
+include_recipe 'letsencrypt::get'
 #include_cookbook 'docker'
 #include_cookbook 'l2tp_ipsec_vpn_server'
-
-include_recipe 'letsencrypt::get'
+include_cookbook 'letsencrypt_renew'

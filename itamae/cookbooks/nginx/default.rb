38e7a8bebd53dbef49f17b89d247a9e711aad5a8
tyage <namatyage@gmail.com>
2017-04-24 16:06:28 +0900

    create dhparam.pem

--- itamae/cookbooks/nginx/default.rb
+++ itamae/cookbooks/nginx/default.rb
@@ -29,3 +29,8 @@ template '/etc/nginx/letsencrypt.conf' do
   group 'root'
   notifies :reload, 'service[nginx]'
 end
+
+execute 'create dhparam.pem' do
+  command 'touch /etc/nginx/dhparam.pem && chown www-data:root /etc/nginx/dhparam.pem && chmod 660 /etc/nginx/dhparam.pem && openssl dhparam -out /etc/nginx/dhparam.pem 2048 && chmod 440 /etc/nginx/dhparam.pem'
+  not_if 'test -e /etc/nginx/dhparam.pem'
+end

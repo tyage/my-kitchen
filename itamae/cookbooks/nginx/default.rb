02487c5e70d4687638c9a0d03b4c05e61a5caa6c
tyage <namatyage@gmail.com>
2017-04-24 14:39:05 +0900

    letsencrypt snipet conf file

--- itamae/cookbooks/nginx/default.rb
+++ itamae/cookbooks/nginx/default.rb
@@ -1,3 +1,9 @@
+node.reverse_merge!(
+  nginx: {
+    letsencrypt_dir: '/var/www/letsencry'
+  }
+)
+
 include_recipe 'apt_source.rb'
 
 packages = %w(nginx-common nginx)
@@ -9,4 +15,17 @@ end
 
 service 'nginx' do
   action [:enable, :start]
+end
+
+letsencrypt_dir = node[:nginx][:letsencrypt_dir]
+execute "mkdir -p #{letsencrypt_dir}" do
+  command "mkdir -p #{letsencrypt_dir}"
+  not_if "test -d #{letsencrypt_dir}"
+end
+template '/etc/nginx/letsencrypt.conf' do
+  action :create
+  mode '0644'
+  owner 'root'
+  group 'root'
+  notifies :reload, 'service[nginx]'
 end

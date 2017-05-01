7ff786fba586ba3eddb4640353dfe29358facea0
tyage <namatyage@gmail.com>
2017-05-01 16:55:48 +0900

    set user for install mac app

--- itamae/cookbooks/mac_apps/default.rb
+++ itamae/cookbooks/mac_apps/default.rb
@@ -1,7 +1,13 @@
+node.reverse_merge!(
+  mac_apps: {
+    username: 'tyage',
+  }
+)
+
 cask_packages = %w(gyazo skype dropbox slack night-owl virtualbox atom tunnelblick firefox google-chrome google-japanese-ime iterm2 vmware-fusion)
 cask_packages.each do |pkg|
   execute "brew cask install #{pkg}" do
-    user username
+    user node[:mac_apps][:username]
     command "brew cask install #{pkg}"
     only_if "brew cask info #{pkg} | grep -qi 'Not Installed'"
   end

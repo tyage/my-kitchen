0001851954170d7bddda66585350559c55a1d38c
tyage <namatyage@gmail.com>
2017-05-27 22:39:29 +0900

    say no to homesick symlink

--- itamae/cookbooks/command_line/default.rb
+++ itamae/cookbooks/command_line/default.rb
@@ -108,7 +108,7 @@ execute 'homesick' do
     gem install homesick
     rbenv rehash
     homesick clone tyage/dotfiles
-    yes | homesick symlink
+    yes n | homesick symlink
   EOS
   not_if "test -d #{home_dir}/.homesick"
 end

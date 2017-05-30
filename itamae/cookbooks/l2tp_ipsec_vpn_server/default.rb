fe2e31d01c1d3117cb34b5ac7b54e11d96f6aa68
tyage <namatyage@gmail.com>
2017-05-31 00:42:26 +0900

    restart vpn server when secret change

--- itamae/cookbooks/l2tp_ipsec_vpn_server/default.rb
+++ itamae/cookbooks/l2tp_ipsec_vpn_server/default.rb
@@ -1,7 +1,10 @@
 include_recipe 'docker::install'
 
 execute 'docker pull hwdsl2/ipsec-vpn-server' do
-  not_if "docker images ipsec-vpn-server | grep -qi 'ipsec-vpn-server'"
+  not_if "docker images hwdsl2/ipsec-vpn-server | grep -qi 'ipsec-vpn-server'"
+end
+
+directory '/usr/local/ipsec-vpn-server' do
 end
 
 template '/usr/local/ipsec-vpn-server/vpn.env' do
@@ -9,11 +12,24 @@ template '/usr/local/ipsec-vpn-server/vpn.env' do
   mode '0644'
   owner 'root'
   group 'root'
-  notifies :run, 'execute[docker restart ipsec-vpn-server]'
+  notifies :run, 'execute[docker restart ipsec-vpn-server]', :immediately
 end
 
 execute 'docker restart ipsec-vpn-server' do
   action :nothing
+  command <<-"EOS"
+    docker stop ipsec-vpn-server &&
+    docker rm ipsec-vpn-server &&
+    docker run \
+      --name ipsec-vpn-server \
+      --env-file /usr/local/ipsec-vpn-server/vpn.env \
+      --restart=always \
+      -p 500:500/udp \
+      -p 4500:4500/udp \
+      -v /lib/modules:/lib/modules:ro \
+      -d --privileged \
+      hwdsl2/ipsec-vpn-server
+  EOS
   only_if "docker container list | grep -qi 'ipsec-vpn-server'"
 end
 
@@ -21,9 +37,8 @@ execute 'docker run ipsec-vpn-server' do
   command <<-"EOS"
     modprobe af_key &&
     docker run \
-      --rm \
       --name ipsec-vpn-server \
-      --env-file ./vpn.env \
+      --env-file /usr/local/ipsec-vpn-server/vpn.env \
       --restart=always \
       -p 500:500/udp \
       -p 4500:4500/udp \

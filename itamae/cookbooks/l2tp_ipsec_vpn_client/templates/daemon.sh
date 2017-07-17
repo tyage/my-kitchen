1fbfa5b1975a305a4bda74f885b9062aed4b420d
tyage <namatyage@gmail.com>
2017-07-18 01:05:07 +0900

    connect to vpn and use dhclient

--- itamae/cookbooks/l2tp_ipsec_vpn_client/templates/daemon.sh
+++ itamae/cookbooks/l2tp_ipsec_vpn_client/templates/daemon.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-DAEMON="#{node[:l2tp_ipsec_vpn_client][:install_directory]}/vpnclient"
-VPNUP=`ps -ef | grep vpnclient | grep -v grep | wc -l`
+DAEMON="<%= node[:l2tp_ipsec_vpn_client][:install_directory] %>/vpnclient"
+VPNCMD="<%= node[:l2tp_ipsec_vpn_client][:install_directory] %>/vpncmd"
+VPNUP=`ps -ef | grep $DAEMON | grep -v grep | wc -l`
 VPNNAME="vpn_vpn"
-GATEWAY="192.168.30.11"
-SUBNET="192.168.30.0/24"
 
 test -x $DAEMON || exit 0
 
 case "$1" in
   start)
-    if [ $VPNUP > 0 ]; then
+    if [ $VPNUP -gt 0 ]; then
       echo "VPN client has already started"
       exit 1
     fi
 
     $DAEMON start
+    $VPNCMD localhost /CLIENT /CMD AccountConnect VPN
+    dhclient $VPNNAME
 
     VPNLINK=`ip -4 -o addr | grep -c vpn_`
     while [ $VPNLINK = 0 ]
@@ -24,8 +25,6 @@ case "$1" in
       sleep 1
       VPNLINK=`ip -4 -o addr | grep -c vpn_`
     done
-
-    route add -net $SUBNET gw $GATEWAY dev $VPNNAME
   ;;
   stop)
     if [ $VPNUP = 0 ]; then
@@ -33,8 +32,7 @@ case "$1" in
       exit 1
     fi
 
-    route del -net $SUBNET gw $GATEWAY dev $VPNNAME
-
+    $VPNCMD localhost /CLIENT /CMD AccountDisconnect VPN
     $DAEMON stop
   ;;
   restart)

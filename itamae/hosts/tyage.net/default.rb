07bf813f10ff1d59fb474f3ecf869eaf3571e52e
tyage <namatyage@gmail.com>
2017-08-20 00:45:48 +0900

    add iptables rule

--- itamae/hosts/tyage.net/default.rb
+++ itamae/hosts/tyage.net/default.rb
@@ -2,8 +2,25 @@ certbot_auto_path = '/usr/bin/certbot-auto'
 
 letsencrypt_dir = '/var/www/letsencrypt'
 node.reverse_merge!(
-  docker: {
-    users: ['tyage']
+  iptables: {
+    iptables_rules: <<-'EOS'
+*filter
+:INPUT ACCEPT [0:0]
+:FORWARD ACCEPT [0:0]
+:OUTPUT ACCEPT [4009:619411]
+:f2b-sshd - [0:0]
+-A INPUT -p tcp -m multiport --dports 22 -j f2b-sshd
+-A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+-A INPUT -p icmp -j ACCEPT
+-A INPUT -i lo -j ACCEPT
+-A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
+-A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+-A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+-A INPUT -j REJECT --reject-with icmp-host-prohibited
+-A FORWARD -j REJECT --reject-with icmp-host-prohibited
+-A f2b-sshd -j RETURN
+COMMIT
+EOS
   },
   letsencrypt: {
     certbot_auto_path: certbot_auto_path,
@@ -30,7 +47,8 @@ node.reverse_merge!(
 
 include_cookbook 'basic'
 include_cookbook 'command_line'
-
+include_cookbook 'locale'
+include_cookbook 'iptables'
 include_cookbook 'nginx'
 include_cookbook 'nginx_letsencrypt'
 include_recipe 'letsencrypt::get'

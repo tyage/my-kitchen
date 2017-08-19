e66c48e645205352979035b21ef23cc35c20071f
tyage <namatyage@gmail.com>
2017-08-19 22:01:12 +0900

    accept all

--- itamae/hosts/mocos.kitchen/default.rb
+++ itamae/hosts/mocos.kitchen/default.rb
@@ -12,10 +12,9 @@ node.reverse_merge!(
 -A INPUT -p tcp -m multiport --dports 22 -j f2b-sshd
 -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 -A INPUT -p icmp -j ACCEPT
+-A INPUT -p tcp -j ACCEPT
+-A INPUT -p udp -j ACCEPT
 -A INPUT -i lo -j ACCEPT
--A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
--A INPUT -p tcp -m comment --comment "accept http" -m tcp --dport 80 -j ACCEPT
--A INPUT -p tcp -m comment --comment "accept https" -m tcp --dport 443 -j ACCEPT
 -A INPUT -j REJECT --reject-with icmp-host-prohibited
 -A FORWARD -j REJECT --reject-with icmp-host-prohibited
 -A f2b-sshd -j RETURN

71e3649a5adf32540baffbfc109c030ae09cfcb3
tyage <namatyage@gmail.com>
2017-08-20 22:22:30 +0900

    create user and database for blog.tyage.net

--- itamae/cookbooks/blog.tyage.net/default.rb
+++ itamae/cookbooks/blog.tyage.net/default.rb
@@ -1,4 +1,11 @@
-packages = %w(php php-fpm mysql)
+node.reverse_merge!(
+  blog_tyage_net: {
+    db_name: 'wordpress',
+    db_user: 'wordpress'
+  }
+)
+
+packages = %w(php php-fpm mysql-server)
 packages.each do |package|
   package package do
     action :install
@@ -16,4 +23,25 @@ end
 git '/var/www/blog.tyage.net/public_html' do
   user 'www-data'
   repository 'https://github.com/WordPress/WordPress.git'
+end
+
+# setup database
+db_name = node['blog_tyage_net']['db_name']
+db_user = node['blog_tyage_net']['db_user']
+db_password = node['blog_tyage_net']['db_password']
+
+execute 'create database' do
+  sql = "CREATE DATABASE IF NOT EXISTS #{db_name} character set utf8"
+  user 'root'
+  command "mysql -u root -e #{Shellwords.escape(sql)}"
+  not_if "mysql -u root -e \"show databases\" | grep #{db_name}"
+end
+execute 'create user' do
+  sql = <<-"EOS"
+CREATE USER '#{db_user}'@'localhost' IDENTIFIED BY '#{db_password}';
+GRANT ALL ON `#{db_name}`.* TO '#{db_user}'@'localhost';
+EOS
+  user 'root'
+  command "mysql -u root -e #{Shellwords.escape(sql)}"
+  not_if "mysql -u root -e \"select user from mysql.user\" | grep #{db_user}"
 end
--- itamae/cookbooks/mysql_server/default.rb
+++ itamae/cookbooks/mysql_server/default.rb
@@ -1,11 +1,11 @@
 package 'mysql-server'
 
 service 'mysql' do
-    action [:start, :enable]
+  action [:start, :enable]
 end
 
 execute 'initialize mysql' do
   user 'root'
   command "mysqladmin -u root password #{Shellwords.escape(node[:mysql_server][:root_password])}"
-  only_if 'mysql -u root -e "show databases" | grep information_schema'
+  not_if 'mysql -u root -e "select authentication_string from mysql.user where user = \'root\'" | grep -v authentication_string'
 end

776d9f58bd440707945e3a386414d144c8e17d15
tyage <namatyage@gmail.com>
2017-08-20 17:26:09 +0900

    deploy tyage.net

--- itamae/cookbooks/tyage.net/default.rb
+++ itamae/cookbooks/tyage.net/default.rb
@@ -8,3 +8,26 @@ sites.each do |site|
     notifies :reload, 'service[nginx]'
   end
 end
+
+%w(nodejs npm).each do |p|
+  package p
+end
+case node[:platform]
+when 'debian', 'ubuntu'
+  package 'nodejs-legacy'
+end
+
+src_dir = '/var/www/tyage.net'
+git src_dir do
+  repository 'https://github.com/tyage/tyage.net'
+  notifies :run, 'execute[build tyage.net]'
+end
+
+execute 'build tyage.net' do
+  cwd src_dir
+  command <<-'EOS'
+npm install
+npm run build
+EOS
+  action :nothing
+end

31bb8c3cd115297c3e2b6f19c5deeeade7db89f1
tyage <namatyage@gmail.com>
2017-09-05 22:37:10 +0900

    store consumer {key, secret}, access token {,secret} for chinachu in itamae-secret

--- itamae/cookbooks/chinachu/default.rb
+++ itamae/cookbooks/chinachu/default.rb
@@ -1,26 +1,43 @@
+node.reverse_merge!(
+  chinachu: {
+    operTweeterAuth: {}
+  }
+)
+
 %w(build-essential curl git-core libssl-dev yasm libtool autoconf).each do |p|
   package p
 end
 
-# clone
-chinachu_src_path = '/home/chinachu/chinachu'
+chinachu_home_path = '/home/chinachu'
+chinachu_src_path = "#{chinachu_home_path}/chinachu"
 
 user 'chinachu' do
-  home '/home/chinachu'
+  home chinachu_home_path
   create_home true
 end
 
+# clone
 git chinachu_src_path do
   user 'chinachu'
   repository 'https://github.com/kanreisa/Chinachu'
 end
 
-remote_file "#{chinachu_src_path}/config.json" do
-  source 'files/config.json'
+config_file = "#{chinachu_src_path}/config.json"
+template config_file do
+  source 'templates/config.json'
+  user 'chinachu'
+  owner 'chinachu'
+  group 'chinachu'
+  not_if "test -e #{config_file}"
 end
 
-remote_file "#{chinachu_src_path}/rules.json" do
+rules_file = "#{chinachu_src_path}/rules.json"
+remote_file rules_file do
   source 'files/rules.json'
+  user 'chinachu'
+  owner 'chinachu'
+  group 'chinachu'
+  not_if "test -e #{rules_file}"
 end
 
 execute 'install chinachu' do
@@ -28,15 +45,16 @@ execute 'install chinachu' do
   user 'chinachu'
   command <<-'EOS'
     echo 1 | ./chinachu installer
-    ./chinachu service operator initscript > /tmp/chinachu-operator
-    ./chinachu service wui initscript > /tmp/chinachu-wui
   EOS
+  not_if "test -d #{chinachu_src_path}/node_modules"
 end
 
 execute 'set init.d of chinachu and start' do
-  cwd '/tmp'
+  cwd chinachu_src_path
   user 'root'
   command <<-'EOS'
+    ./chinachu service operator initscript > /tmp/chinachu-operator
+    ./chinachu service wui initscript > /tmp/chinachu-wui
     chown root:root /tmp/chinachu-operator /tmp/chinachu-wui
     chmod +x /tmp/chinachu-operator /tmp/chinachu-wui
     mv /tmp/chinachu-operator /tmp/chinachu-wui /etc/init.d/
@@ -47,4 +65,5 @@ execute 'set init.d of chinachu and start' do
     service chinachu-operator start
     service chinachu-wui start
   EOS
+  not_if "test -e /etc/init.d/chinachu-operator"
 end

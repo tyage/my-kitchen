f2c0f72f7291468f6445f8c6d679b388d5ec08e2
tyage <keitaro.yamazaki@linecorp.com>
2018-05-15 21:20:18 +0900

    update softether client

--- itamae/cookbooks/l2tp_ipsec_vpn_client/default.rb
+++ itamae/cookbooks/l2tp_ipsec_vpn_client/default.rb
@@ -7,7 +7,7 @@ node.reverse_merge!(
     user: node[:secrets][:l2tp_ipsec_vpn_user],
     user_password: node[:secrets][:l2tp_ipsec_vpn_password],
     install_directory: '/home/tyage/.vpnclient',
-    softether_download_url: 'http://jp.softether-download.com/files/softether/v4.22-9634-beta-2016.11.27-tree/Linux/SoftEther_VPN_Client/64bit_-_Intel_x64_or_AMD64/softether-vpnclient-v4.22-9634-beta-2016.11.27-linux-x64-64bit.tar.gz'
+    softether_download_url: 'http://jp.softether-download.com/files/softether/v4.27-9666-beta-2018.04.21-tree/Linux/SoftEther_VPN_Client/64bit_-_Intel_x64_or_AMD64/softether-vpnclient-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz'
   }
 )
 

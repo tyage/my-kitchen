6a6be5311cd62dd685c36ac66e46d634263a0878
tyage <namatyage@gmail.com>
2018-05-18 21:05:59 +0900

    Update setup-ubuntu.sh

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt install ruby git libsodium18
-git clone git@github.com:tyage/my-kitchen.git
+git clone https://github.com/tyage/my-kitchen.git
 cd my-kitchen
 gem install bundle
 bundle i --path=vendor/bundle

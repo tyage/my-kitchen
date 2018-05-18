93b842561d976397aaa53b74e19c8699b0f0d8ee
tyage <namatyage@gmail.com>
2018-05-18 21:34:20 +0900

    Update setup-ubuntu.sh

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install ruby git libsodium18 build-essential libffi-dev python-dev
+sudo apt install ruby ruby-dev gcc make git libsodium18 build-essential libffi-dev python-dev
 git clone https://github.com/tyage/my-kitchen.git
 cd my-kitchen
 sudo gem install bundle

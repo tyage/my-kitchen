8e724eec2616e45e150402f74bddc0f1cf4ab535
tyage <namatyage@gmail.com>
2018-05-26 21:15:56 +0900

    change user

--- itamae/cookbooks/chinachu/default.rb
+++ itamae/cookbooks/chinachu/default.rb
@@ -1,8 +1,7 @@
 node.reverse_merge!(
   chinachu: {
-    user: 'tyage',
     operTweeterAuth: {},
-    src_path: '/usr/local/docker-mirakurun-chinachu'
+    src_path: '/usr/local/src/docker-mirakurun-chinachu'
   }
 )
 
@@ -12,34 +11,33 @@ src_path = node[:chinachu][:src_path]
 include_recipe "docker::install"
 
 git src_path do
-  user user
+  user 'root'
   repository 'https://github.com/Chinachu/docker-mirakurun-chinachu.git'
 end
 
 config_file = "#{src_path}/chinachu/conf/config.json"
 template config_file do
   source 'templates/config.json'
-  user user
-  owner user
-  group user
+  user 'root'
+  owner 'root'
+  group 'root'
   not_if "test -s #{config_file}"
 end
 
 docker_file = "#{src_path}/docker-compose.yml"
 remote_file docker_file do
   source 'files/docker-compose.yml'
-  user user
-  owner user
-  group user
-  not_if "test -e #{docker_file}"
+  user 'root'
+  owner 'root'
+  group 'root'
 end
 
 rules_file = "#{src_path}/chinachu/conf/rules.json"
 remote_file rules_file do
   source 'files/rules.json'
-  user user
-  owner user
-  group user
+  user 'root'
+  owner 'root'
+  group 'root'
   not_if "test -e #{rules_file}"
 end
 
@@ -48,9 +46,9 @@ service_files.each do |file|
   distination = "/etc/systemd/system/#{file}"
   template distination do
     source "templates/#{file}"
-    user user
-    owner user
-    group user
+    user 'root'
+    owner 'root'
+    group 'root'
     notifies :reload, "service[#{file}]"
   end
   service file do
